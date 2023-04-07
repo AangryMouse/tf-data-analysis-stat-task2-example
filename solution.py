359ef8ab7c578410c4884234a6f36a69b77b0cfd
AangryMouse <71213909+AangryMouse@users.noreply.github.com>
2023-04-08 02:03:45 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,9 +8,10 @@ chat_id = 396317433
 
 def solution(p: float, x: np.array) -> tuple:
     n = x.shape[0]
-    alpha = p
-    loc = x.mean() * 2
-    scale = loc / np.sqrt(3*n)
-    return loc + 0.038 - norm.ppf((1 + alpha) / 2) * scale, \
-           loc + 0.038 + norm.ppf((1 + alpha) / 2) * scale
+    alpha = 1 - p
+    loc = x.mах()
+    scale = (loc - 0.038) / np.sqrt(2*n)
+    return loc - norm.ppf(1 - alpha / 2) * scale, \
+           loc + norm.ppf(1 - alpha / 2) * scale
+
 
